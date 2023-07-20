4f184f0de50fb594f23672b85db4c964fd5de404
kirillkondratev <125269901+kirillkondratev@users.noreply.github.com>
2023-07-20 21:01:12 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -4,29 +4,19 @@ from customers
 ;
 
 --this query counts top 10 total income
-with tab as (
-select sales_id,
-	   concat(e.first_name||' '||e.last_name) as name1,
-	   quantity,
-	   p.price,
-	   quantity*price as sale_amount,
-	   sale_date,
-	   s.product_id,
-	   p.name
+select 
+	   concat(e.first_name||' '||e.last_name) as name,
+	   count(s.sales_id) as operations,
+	   sum(quantity*price) as income
 from sales as s
 	left join products as p 
 	on s.product_id = p.product_id
 	left join employees as e 
 	on s.sales_person_id = e.employee_id
-order by sales_person_id)
-
-select name1 as name,
-	   count(sales_id) as operations,
-	   sum(sale_amount) as income
-from tab
-group by name1
+group by concat(e.first_name||' '||e.last_name)
 order by income desc
-limit 10;
+limit 10
+;
 
 --this query counts lowest average income
 with tab as (
@@ -87,80 +77,46 @@ group by day_number,weekday,name1
 order by day_number,name1; 
    
 --this query shows age groups
-with tab as (
-select *,
-	   (case when age <= 25 then '16-25'
-	   when age > 25 and age <= 40 then '26-40'
-	   else '40+'
-	   end) as age_category
+select 
+	(case 
+		when age <= 25 then '16-25'
+	   	when age > 25 and age <= 40 then '26-40'
+	   	else '40+'
+	 end) as age_category,
+	 count (customer_id) as count
 from customers
-order by age asc)
-
-select age_category,
-	   count(age)
-from tab
 group by age_category
-order by age_category;
+order by count
+;
 
 --this query shows customers by month
 
-with tab as (
 
-select sales_id,
-	   sale_date,
+select 
 	   to_char(sale_date, 'YYYY-MM') as date,
-	   concat(c.first_name||' '||c.last_name) as customers_name,
-	   s.quantity*p.price as income
+	   count(distinct s.customer_id) as total_customers,
+	   sum(s.quantity*p.price) as income
 from sales as s
 left join customers as c
 on s.customer_id = c.customer_id
 left join products as p
 on s.product_id = p.product_id
-order by sale_date
-
-)
-
-select date,
-	   count(distinct customers_name) as total_customers,
-	   sum(income) as income
-from tab
 group by date
 ;
 
 --this query shows first special offer customers
 
-with tab as (
-select c.customer_id,
-	   concat (c.first_name||' '||c.last_name) as customer,
-	   sale_date,
-	   p.name,
-	   price,
-	   concat(e.first_name||' '||e.last_name) as seller
+select 
+	distinct on (concat (c.first_name||' '||c.last_name)) concat (c.first_name||' '||c.last_name) as customer,
+	sale_date,
+	concat(e.first_name||' '||e.last_name) as seller
 from sales as s
 left join customers as c 
-	on s.customer_id = c.customer_id
+	on s.customer_id = c.customer_id 
 left join products as p
-	on s.product_id = p.product_id
-left join employees as e
+	on s.product_id = p.product_id 
+left join employees as e 
 	on s.sales_person_id = e.employee_id
-order by customer_id, sale_date
-), tab2 as (
-
-select customer_id,
-	   customer,
-	   sale_date,
-	   seller,
-	   first_value (price) over (partition by customer) as f_v,
-	   first_value (sale_date) over (partition by customer order by sale_date) as s_d
-from tab
-order by customer_id, sale_date
-)
-select distinct on 
-	   (customer) customer,
-	   s_d as sale_date,
-	   seller
-from tab2
-where f_v = 0
-order by customer
-;
-	   
+where price = 0
+order by customer, sale_date
+;  
